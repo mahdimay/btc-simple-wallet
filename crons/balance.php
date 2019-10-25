68a4a4bec157d13244e956cbe4eae23737fea9e9
houssemcharf <houssem.charf@gmail.com>
2019-10-25 02:22:43 +0100

    Escaping sql injection potential

--- crons/balance.php
+++ crons/balance.php
@@ -4,11 +4,11 @@ $sql = "SELECT user FROM users";
 $result = $con->query($sql);
 $balance = 0;
 while ($row = $result->fetch_assoc()){
-    $user = $row['user'];
+    $user = mysql_real_escape_string($row['user']);
     $sql2 = "SELECT address FROM address WHERE user='$user'";
     $result2 = $con->query($sql2);
     while ($row2 = $result2->fetch_assoc()){
-        $address = $row2['address'];
+        $address = mysql_real_escape_string($row2['address']);
         $url = "https://chain.so/api/v2/get_address_balance/BTC/$address";
         $decoded = json_decode($url, TRUE);
         $confirmed = $decoded['data']['confirmed_balance'];
